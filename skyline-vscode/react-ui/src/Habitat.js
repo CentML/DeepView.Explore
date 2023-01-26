c7bc90cd3cd8dce51b50ab5b845a1054da1d06fa
John Calderon <johncalesp@gmail.com>
2023-01-26 16:46:58 -0500

    deleted habitat comments

--- skyline-vscode/react-ui/src/Habitat.js
+++ skyline-vscode/react-ui/src/Habitat.js
@@ -29,30 +29,6 @@ export default function Habitat({habitatData}) {
         '#ffc300'
     ];
 
-    // const sampleData = [
-    //     {y: 'device 1', x: 10, color: 0},
-    //     {y: 'device 2', x: 5, color: 1},
-    //     {y: 'device 3', x: 15, color: 2},
-    //     {y: 'device 4', x: 20, color: 3}
-    // ];
-
-    // const sampleDataVGG = [
-    //     {y: "V100", x: 38.6031339393715, color: 0},
-    //     {y: "P100", x: 67.57987999101333, color: 1},
-    //     {y: "T4", x: 118.20070742178441, color: 2},
-    //     {y: "P4000", x: 145.73762440806306, color: 3},
-    //     {y: "RTX2070", x: 74.75586879514157, color: 4}
-    // ];
-
-    // // Resnet
-    // const sampleDataResnet = [
-    //     {y: "V100", x: 47.40975887490825, color: 0},
-    //     {y: "P100", x: 81.04154092289134, color: 1},
-    //     {y: "T4", x: 124.23241486678548, color: 2},
-    //     {y: "P4000", x: 173.1962974218274, color: 3},
-    //     {y: "RTX2070", x: 90.15023999989465, color: 4}
-    // ];
-
     let sampleData = [];
 
     for (var i = 0; i < habitatData.length; i++) {
