de850e9ce8552bfcc3978c3685ebc8080c377330
John Calderon <johncalesp@gmail.com>
2023-01-31 17:03:02 -0500

    changes to provider panel | and moved static data to provider.js

--- skyline-vscode/react-ui/src/ProviderPanel.js
+++ skyline-vscode/react-ui/src/ProviderPanel.js
@@ -1,3 +1,4 @@
+import React,{useState, useEffect} from "react";
 import Subheader from "./Subheader";
 import {
   VerticalGridLines,
@@ -19,11 +20,44 @@ import {
   Image,
   Table,
 } from "react-bootstrap";
-import React,{useState, useEffect} from "react";
+
+import { find_card_in_habitat } from "./utils";
+import { instances,cardMemory } from "./data/providers";
+
+const normalColor = "#8DD0FF";
+const highlightColor = "#44C96D";
+const normalSize = 8;
+const enlargedSize = 12;
+
+const populate_initial_data = (habitatData)=>{
+  let filtered_instances=[]
+  if(habitatData){
+    for(const instance of instances){
+      const found_in_habitat = habitatData.find((item)=> item[0].toLowerCase() === instance.gpu.toLowerCase())
+      const found_in_cardMemory = cardMemory.find((item)=> item.name.toLocaleLowerCase() === instance.gpu.toLocaleLowerCase())
+      if (found_in_habitat && found_in_cardMemory){
+        filtered_instances.push({
+          id: instance.id,
+          x: found_in_habitat[1], // msec
+          y: instance.cost / (3.6e6) * found_in_habitat[1], // cost per msec * habitatData
+          size: normalSize,
+          color: normalColor,
+          info: instance,
+          vmem: found_in_cardMemory.vmem
+        })
+      }
+    }
+  
+  }
+  console.log(filtered_instances)
+  return filtered_instances;
+}
+
+
 
 const ProviderPanel = ({numIterations,habitatData,changeParentState}) => {
     const [providerPanelSettings, setProviderPanelSettings] = useState({
-              plotData: [],
+              plotData: populate_initial_data(habitatData),
               nearest: null,
               clicked: null,
               maxNumGpu: '4',
@@ -31,10 +65,7 @@ const ProviderPanel = ({numIterations,habitatData,changeParentState}) => {
               estimated_time: 0,
             })
     
-    const normalColor = "#8DD0FF";
-    const highlightColor = "#44C96D";
-    const normalSize = 8;
-    const enlargedSize = 12;
+    
 
     const providers = {
       google: { name: "Google Cloud Platform", logo: "resources/google.png" },
@@ -45,336 +76,135 @@ const ProviderPanel = ({numIterations,habitatData,changeParentState}) => {
 
     // perf obtained from https://cloud.google.com/blog/topics/developers-practitioners/benchmarking-rendering-software-compute-engine
     // assume k80 half as fast as p4
-    const gpus = {
-      t4: { vmem: 16, perf: 1 / 0.3717 },
-      p4: { vmem: 8, perf: 1 / 0.605 },
-      v100: { vmem: 16, perf: 1 / 0.1472 },
-      p100: { vmem: 16, perf: 1 / 0.2253 },
-      a100: { vmem: 40, perf: 1 / 0.1147 },
-      k80: { vmem: 24, perf: 0.5 / 0.605 },
-      rtx2080ti: { vmem: 11, perf: 0.8 * (1 / 0.1472) },
-    };
+    /**
+     * 
+     * this.gpus = {
+            t4: { vmem: 16, perf: 1/0.3717 },
+            p4: { vmem: 8, perf: 1/0.605},
+            v100: { vmem: 16, perf: 1/0.1472 },
+            p100: { vmem: 16, perf: 1/0.2253 },
+            a100: { vmem: 40, perf: 1/0.1147 },
+            k80: { vmem: 24, perf: 0.5/0.605 },
+            rtx2080ti: {vmem: 11, perf: 0.8*(1/0.1472)}
+        };
+     */
 
-    // costs are 1 GPU * hr
-    const instances = [
-      {
-        id: 0,
-        provider: "google",
-        ngpus: 1,
-        instance: "a2-highgpu-1g",
-        gpu: "a100",
-        cost: 2.939,
-      },
-      {
-        id: 1,
-        provider: "google",
-        ngpus: 1,
-        instance: "nvidia-tesla-t4",
-        gpu: "t4",
-        cost: 0.35,
-      },
-      {
-        id: 2,
-        provider: "google",
-        ngpus: 1,
-        instance: "nvidia-tesla-p4",
-        gpu: "p4",
-        cost: 0.6,
-      },
-      {
-        id: 3,
-        provider: "google",
-        ngpus: 1,
-        instance: "nvidia-tesla-v100",
-        gpu: "v100",
-        cost: 2.48,
-      },
-      {
-        id: 4,
-        provider: "google",
-        ngpus: 1,
-        instance: "nvidia-tesla-p100",
-        gpu: "p100",
-        cost: 1.46,
-      },
-      {
-        id: 5,
-        provider: "google",
-        ngpus: 1,
-        instance: "nvidia-tesla-k80",
-        gpu: "k80",
-        cost: 0.45,
-      },
+    const initial_data = populate_initial_data(habitatData);
+    
+    // const gpus = {
+    //   t4: { vmem: 16, perf: 1 / 0.3717, time_per_iter: find_card_in_habitat(habitatData,'t4') },
+    //   p4: { vmem: 8, perf: 1 / 0.605, time_per_iter: find_card_in_habitat(habitatData,'p4') },
+    //   v100: { vmem: 16, perf: 1 / 0.1472, time_per_iter: find_card_in_habitat(habitatData,'v100') },
+    //   p100: { vmem: 16, perf: 1 / 0.2253, time_per_iter: find_card_in_habitat(habitatData,'p100') },
+    //   a100: { vmem: 40, perf: 1 / 0.1147, time_per_iter: find_card_in_habitat(habitatData,'a100') },
+    //   k80: { vmem: 24, perf: 0.5 / 0.605, time_per_iter: find_card_in_habitat(habitatData,'p4') },
+    //   rtx2080ti: { vmem: 11, perf: 0.8*(1/0.1472), time_per_iter: find_card_in_habitat(habitatData,'rtx2080ti')},
+    // };
+    // console.log(gpus);
 
-      {
-        id: 6,
-        provider: "aws",
-        ngpus: 1,
-        instance: "p3.2xlarge",
-        gpu: "v100",
-        cost: 3.06,
-      },
-      {
-        id: 7,
-        provider: "aws",
-        ngpus: 1,
-        instance: "p2.xlarge",
-        gpu: "k80",
-        cost: 0.9,
-      },
-      {
-        id: 8,
-        provider: "aws",
-        ngpus: 1,
-        instance: "g4dn.xlarge",
-        gpu: "t4",
-        cost: 0.526,
-      },
-
-      {
-        id: 9,
-        provider: "azure",
-        ngpus: 1,
-        instance: "NC6",
-        gpu: "k80",
-        cost: 0.9,
-      },
-      {
-        id: 10,
-        provider: "azure",
-        ngpus: 1,
-        instance: "NC6s v2",
-        gpu: "p100",
-        cost: 2.07,
-      },
-      {
-        id: 11,
-        provider: "azure",
-        ngpus: 1,
-        instance: "NC6s v3",
-        gpu: "v100",
-        cost: 3.06,
-      },
-      {
-        id: 12,
-        provider: "azure",
-        ngpus: 1,
-        instance: "NC4as T4 v3",
-        gpu: "t4",
-        cost: 0.526,
-      },
-
-      {
-        id: 13,
-        provider: "centml",
-        ngpus: 1,
-        instance: "CentML",
-        gpu: "rtx2080ti",
-        cost: 0.2,
-      },
-
-      // 2/4 GPU instances
-
-      {
-        id: 14,
-        provider: "google",
-        ngpus: 2,
-        instance: "a2-highgpu-2g",
-        gpu: "a100",
-        cost: 2 * 2.939,
-      },
-      {
-        id: 15,
-        provider: "google",
-        ngpus: 2,
-        instance: "nvidia-tesla-t4",
-        gpu: "t4",
-        cost: 2 * 0.35,
-      },
-      {
-        id: 16,
-        provider: "google",
-        ngpus: 2,
-        instance: "nvidia-tesla-p4",
-        gpu: "p4",
-        cost: 2 * 0.6,
-      },
-      {
-        id: 17,
-        provider: "google",
-        ngpus: 2,
-        instance: "nvidia-tesla-v100",
-        gpu: "v100",
-        cost: 2 * 2.48,
-      },
-      {
-        id: 18,
-        provider: "google",
-        ngpus: 2,
-        instance: "nvidia-tesla-p100",
-        gpu: "p100",
-        cost: 2 * 1.46,
-      },
-      {
-        id: 19,
-        provider: "google",
-        ngpus: 2,
-        instance: "nvidia-tesla-k80",
-        gpu: "k80",
-        cost: 2 * 0.45,
-      },
-
-      {
-        id: 20,
-        provider: "aws",
-        ngpus: 2,
-        instance: "g4ad.8xlarge",
-        gpu: "t4",
-        cost: 1.734,
-      },
-
-      {
-        id: 21,
-        provider: "google",
-        ngpus: 4,
-        instance: "a2-highgpu-2g",
-        gpu: "a100",
-        cost: 4 * 2.939,
-      },
-      {
-        id: 22,
-        provider: "google",
-        ngpus: 4,
-        instance: "nvidia-tesla-t4",
-        gpu: "t4",
-        cost: 4 * 0.35,
-      },
-      {
-        id: 23,
-        provider: "google",
-        ngpus: 4,
-        instance: "nvidia-tesla-p4",
-        gpu: "p4",
-        cost: 4 * 0.6,
-      },
-      {
-        id: 24,
-        provider: "google",
-        ngpus: 4,
-        instance: "nvidia-tesla-v100",
-        gpu: "v100",
-        cost: 4 * 2.48,
-      },
-      {
-        id: 25,
-        provider: "google",
-        ngpus: 4,
-        instance: "nvidia-tesla-p100",
-        gpu: "p100",
-        cost: 4 * 1.46,
-      },
-      {
-        id: 26,
-        provider: "google",
-        ngpus: 4,
-        instance: "nvidia-tesla-k80",
-        gpu: "k80",
-        cost: 4 * 0.45,
-      },
-
-      {
-        id: 27,
-        provider: "aws",
-        ngpus: 4,
-        instance: "p3.8xlarge",
-        gpu: "v100",
-        cost: 12.24,
-      },
-      {
-        id: 28,
-        provider: "aws",
-        ngpus: 4,
-        instance: "g4dn.12xlarge",
-        gpu: "t4",
-        cost: 3.912,
-      },
-    ];
+    
 
   const populateProviderGraph = (maxGpus) => {
     
-    let newPlotData = [];
-    for (let i = 0; i < instances.length; i++) {
-      let perf =
-        gpus[instances[i].gpu].perf *
-        instances[i].ngpus *
-        (1114.12 / 341.6241 / 4);
-      if (instances[i].ngpus <= Number(maxGpus)) {
-        newPlotData.push({
-          id: instances[i].id,
-          x: 1.0 / perf,
-          y: instances[i].cost / perf,
-          size: normalSize,
-          color: normalColor,
-          info: instances[i],
-        });
-      }
-    }
+    // let newPlotData = [];
+    // for (let i = 0; i < instances.length; i++) {
+    //   let perf =
+    //     gpus[instances[i].gpu].time_per_iter || gpus[instances[i].gpu].perf *
+    //     instances[i].ngpus *
+    //     (1114.12 / 341.6241 / 4);
+    //   if (instances[i].ngpus <= Number(maxGpus)) {
+    //     newPlotData.push({
+    //       id: instances[i].id,
+    //       x: 1.0 / perf,
+    //       y: instances[i].cost / perf,
+    //       size: normalSize,
+    //       color: normalColor,
+    //       info: instances[i],
+    //     });
+    //   }
+    // }
     setProviderPanelSettings((prevState)=>({
         ...prevState,
         maxNumGpu: maxGpus,
-        plotData: newPlotData,})
+        plotData: initial_data?.filter((item)=>item.info.ngpus<=maxGpus)})
     )
   };
 
-  const recalculateCost = () => {
-    let thisGpu = providerPanelSettings.clicked;
-    if (thisGpu == null) return;
+  const recalculateCost = (provider) => {
+    // let thisGpu = providerPanelSettings.clicked;
+    // console.log(thisGpu);
+    if (provider == null) return;
     // V100: 40ms
     // Relative per-iteration time is 40 * perf(v100) / perf(current)
-    let iters = numIterations;
-    let perf =
-      gpus[thisGpu.info.gpu].perf *
-      thisGpu.info.ngpus *
-      (1114.12 / 341.6241 / 4);
-    let perIterMs = (40 * (1 / 0.1472)) / perf;
-    let totalHr = (iters * perIterMs) / 3.6e6;
-    let totalCost = thisGpu.info.cost * totalHr;
+    // let iters = numIterations;
+    // let perf =
+    //   gpus[thisGpu.info.gpu].time_per_iter || gpus[thisGpu.info.gpu].perf *
+    //   thisGpu.info.ngpus *
+    //   (1114.12 / 341.6241 / 4);
+    // let perIterMs = (40 * (1 / 0.1472)) / perf;
+    // let totalHr = (iters * perIterMs) / 3.6e6;
+    // let totalCost = thisGpu.info.cost * totalHr;
+    let totalHr = (numIterations * provider.x) / 3.6e6;
+    let totalCost = provider.info.cost * totalHr;
 
     setProviderPanelSettings((prevState)=>({
-      ...prevState,   
+      ...prevState,
+      clicked: provider,   
       estimated_cost: totalCost,
       estimated_time: totalHr,
     }));
   }
 
   const onClickConfig = (value) => {
-    setProviderPanelSettings((prevState)=>({ ...prevState, clicked: value }));
+    console.log(value);
 
-    for (let i = 0; i < providerPanelSettings.plotData.length; i++) {
-        providerPanelSettings.plotData[i].color = normalColor;
-        providerPanelSettings.plotData[i].size = normalSize;
-    }
-    providerPanelSettings.plotData[value.id].color = highlightColor;
-    providerPanelSettings.plotData[value.id].size = enlargedSize;
+    //setProviderPanelSettings((prevState)=>({ ...prevState, clicked: value }));
 
-    changeParentState((prevState)=>({ ...prevState, btnDeployVariant: "primary" }));
-
-    // V100: 40ms
-    // Relative per-iteration time is 40 * perf(v100) / perf(current)
-    let iters = numIterations;
-    let perf =
-      gpus[value.info.gpu].perf *
-      value.info.ngpus *
-      (1114.12 / 341.6241 / 4);
-    let perIterMs = (40 * (1 / 0.1472)) / perf;
-    let totalHr = (iters * perIterMs) / 3.6e6;
-    let totalCost = value.info.cost * totalHr;
+    // const change_plot_data = providerPanelSettings.plotData;
+    
+    // for (let i = 0; i < change_plot_data.length; i++) {
+    //   change_plot_data[i].color = normalColor;
+    //   change_plot_data[i].size = normalSize;
+    // }
+    // change_plot_data[value.id].color = highlightColor;
+    // change_plot_data[value.id].size = enlargedSize;
 
     setProviderPanelSettings((prevState)=>({
-        ...prevState,   
-        estimated_cost: totalCost,
-        estimated_time: totalHr,
-      }));;
+      ...prevState,
+      plotData: prevState.plotData.map((item) => {
+        if(item.id === value.id){
+          return {
+            ...item,
+            color:highlightColor,
+            size:enlargedSize
+          }
+        }
+        return {
+          ...item,
+          color:normalColor,
+          size:normalSize
+        }
+      })
+    }))
+    changeParentState((prevState)=>({ ...prevState, btnDeployVariant: "primary" }));
+
+    // // V100: 40ms
+    // // Relative per-iteration time is 40 * perf(v100) / perf(current)
+    // let iters = numIterations;
+    // let perf =
+    //   gpus[value.info.gpu].perf *
+    //   value.info.ngpus *
+    //   (1114.12 / 341.6241 / 4);
+    // let perIterMs = (40 * (1 / 0.1472)) / perf;
+    // let totalHr = (iters * perIterMs) / 3.6e6;
+    // let totalCost = value.info.cost * totalHr;
+
+    // setProviderPanelSettings((prevState)=>({
+    //     ...prevState,   
+    //     estimated_cost: totalCost,
+    //     estimated_time: totalHr,
+    //   }));;
+    recalculateCost(value);
   }
 
     useEffect(()=>{
@@ -384,7 +214,7 @@ const ProviderPanel = ({numIterations,habitatData,changeParentState}) => {
     },[])
 
     useEffect(() => {
-        recalculateCost();
+        recalculateCost(providerPanelSettings.clicked);
     // eslint-disable-next-line react-hooks/exhaustive-deps
     },[numIterations])
 
@@ -484,7 +314,6 @@ const ProviderPanel = ({numIterations,habitatData,changeParentState}) => {
                               <th>GPU</th>
                               <th>Num. GPU</th>
                               <th>VRAM</th>
-                              <th>Rel. Performance</th>
                             </tr>
                           </thead>
                           <tbody>
@@ -492,12 +321,8 @@ const ProviderPanel = ({numIterations,habitatData,changeParentState}) => {
                               <th>{providerPanelSettings.clicked.info.gpu}</th>
                               <th>{providerPanelSettings.clicked.info.ngpus}</th>
                               <th>
-                                {gpus[providerPanelSettings.clicked.info.gpu].vmem} GB
-                              </th>
-                              <th>
-                                {gpus[
-                                  providerPanelSettings.clicked.info.gpu
-                                ].perf.toFixed(2)}
+                                {/* {gpus[providerPanelSettings.clicked.info.gpu].vmem} GB */}
+                                {providerPanelSettings.clicked.vmem} GB
                               </th>
                             </tr>
                           </tbody>
