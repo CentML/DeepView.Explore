3353d75d253500becd4ae1a7b6a92918f3f54225
John Calderon <johncalesp@gmail.com>
2023-02-07 17:04:14 -0500

    changes to Habitat graph to reflect the latest changes coming from the backend | changed cloud provider instances and references | made some modifications to mock data

--- skyline-vscode/react-ui/src/components/HorizontalBarGraph.js
+++ skyline-vscode/react-ui/src/components/HorizontalBarGraph.js
@@ -10,6 +10,12 @@ import {
 } from "recharts";
 
 const BarGraph = ({ data, height, xlabel, ylabel, color }) => {
+  let upperLimit = 0;
+  data.forEach(element => {
+    const predictedTime = parseFloat(Number(element.x));
+    upperLimit = predictedTime > upperLimit ? predictedTime: upperLimit
+  });
+  upperLimit *=1.1;
   return (
     <ResponsiveContainer width="80%" height={height}>
       <BarChart
@@ -18,7 +24,7 @@ const BarGraph = ({ data, height, xlabel, ylabel, color }) => {
         margin={{ top: 10, right: 30, left: 50, bottom: 50 }}
       >
         
-        <XAxis type="number" label={{ value: xlabel, position: 'insideBottom', offset:-15}}/>
+        <XAxis type="number" domain={[0, Math.floor(upperLimit)]} label={{ value: xlabel, position: 'insideBottom', offset:-15}}/>
         <YAxis type="category" dataKey="y" label={{ value: ylabel, angle: -90, position: 'outside'}}/>
         <Tooltip />
         <Bar  dataKey="x" fill={color}/>
--- skyline-vscode/react-ui/src/data/providers.js
+++ skyline-vscode/react-ui/src/data/providers.js
@@ -1,5 +1,23 @@
-// GPU Instances
-// costs are 1 GPU * hr
+/**
+ * GPU Instances
+ * costs are 1 GPU * hr
+ * For Google we need to add the cost for the instance itself, a100 run on specific A2 Series
+ * while t4, p4, v100, and p100 run on General purposes N1 Series
+ * 
+ * References:
+ * FOR AWS:
+ * https://aws.amazon.com/ec2/instance-types/
+ * https://aws.amazon.com/ec2/pricing/on-demand/
+ * 
+ * FOR GOOGLE:
+ * https://cloud.google.com/compute/gpus-pricing
+ * https://cloud.google.com/products/calculator/
+ * 
+ * FOR AZURE:
+ * https://learn.microsoft.com/en-us/azure/virtual-machines/sizes-gpu
+ * https://azure.microsoft.com/en-ca/pricing/details/virtual-machines/windows/#pricing
+ * 
+ */
 export const cloudInstances = [
   {
     id: 0,
@@ -7,31 +25,31 @@ export const cloudInstances = [
     ngpus: 1,
     instance: "a2-highgpu-1g",
     gpu: "a100",
-    cost: 2.939,
+    cost: 2.93 + 0.74,
   },
   {
     id: 1,
     provider: "google",
     ngpus: 1,
-    instance: "nvidia-tesla-t4",
+    instance: "n1-standard-1",
     gpu: "t4",
-    cost: 0.35,
+    cost: 0.35 + 0.05,
   },
   {
     id: 2,
     provider: "google",
     ngpus: 1,
-    instance: "nvidia-tesla-p4",
+    instance: "n1-standard-1",
     gpu: "p4",
-    cost: 0.6,
+    cost: 0.6 + 0.05,
   },
   {
     id: 3,
     provider: "google",
     ngpus: 1,
-    instance: "nvidia-tesla-v100",
+    instance: "n1-standard-1",
     gpu: "v100",
-    cost: 2.48,
+    cost: 2.48 + 0.05,
   },
   {
     id: 4,
@@ -39,17 +57,8 @@ export const cloudInstances = [
     ngpus: 1,
     instance: "nvidia-tesla-p100",
     gpu: "p100",
-    cost: 1.46,
+    cost: 1.46 + 0.05,
   },
-  {
-    id: 5,
-    provider: "google",
-    ngpus: 1,
-    instance: "nvidia-tesla-k80",
-    gpu: "k80",
-    cost: 0.45,
-  },
-
   {
     id: 6,
     provider: "aws",
@@ -59,29 +68,12 @@ export const cloudInstances = [
     cost: 3.06,
   },
   {
-    id: 7,
-    provider: "aws",
-    ngpus: 1,
-    instance: "p2.xlarge",
-    gpu: "k80",
-    cost: 0.9,
-  },
-  {
     id: 8,
     provider: "aws",
     ngpus: 1,
     instance: "g4dn.xlarge",
     gpu: "t4",
     cost: 0.526,
-  },
-
-  {
-    id: 9,
-    provider: "azure",
-    ngpus: 1,
-    instance: "NC6",
-    gpu: "k80",
-    cost: 0.9,
   },
   {
     id: 10,
@@ -125,107 +117,89 @@ export const cloudInstances = [
     ngpus: 2,
     instance: "a2-highgpu-2g",
     gpu: "a100",
-    cost: 2 * 2.939,
+    cost: 5.87 + 1.48,
   },
   {
     id: 15,
     provider: "google",
     ngpus: 2,
-    instance: "nvidia-tesla-t4",
+    instance: "n1-standard-1",
     gpu: "t4",
-    cost: 2 * 0.35,
+    cost: 2 * 0.35 + 0.05,
   },
   {
     id: 16,
     provider: "google",
     ngpus: 2,
-    instance: "nvidia-tesla-p4",
+    instance: "n1-standard-1",
     gpu: "p4",
-    cost: 2 * 0.6,
+    cost: 2 * 0.6 + 0.05,
   },
   {
     id: 17,
     provider: "google",
     ngpus: 2,
-    instance: "nvidia-tesla-v100",
+    instance: "n1-standard-1",
     gpu: "v100",
-    cost: 2 * 2.48,
+    cost: 2 * 2.48 + 0.05,
   },
   {
     id: 18,
     provider: "google",
     ngpus: 2,
-    instance: "nvidia-tesla-p100",
+    instance: "n1-standard-1",
     gpu: "p100",
-    cost: 2 * 1.46,
+    cost: 2 * 1.46 + 0.05,
   },
-  {
-    id: 19,
-    provider: "google",
-    ngpus: 2,
-    instance: "nvidia-tesla-k80",
-    gpu: "k80",
-    cost: 2 * 0.45,
-  },
-
   {
     id: 20,
     provider: "aws",
-    ngpus: 2,
-    instance: "g4ad.8xlarge",
+    ngpus: 4,
+    instance: "g4dn.12xlarge",
     gpu: "t4",
-    cost: 1.734,
+    cost: 3.912,
   },
 
   {
     id: 21,
     provider: "google",
     ngpus: 4,
-    instance: "a2-highgpu-2g",
+    instance: "a2-highgpu-4g",
     gpu: "a100",
-    cost: 4 * 2.939,
+    cost: 11.74 + 2.96,
   },
   {
     id: 22,
     provider: "google",
     ngpus: 4,
-    instance: "nvidia-tesla-t4",
+    instance: "n1-standard-1",
     gpu: "t4",
-    cost: 4 * 0.35,
+    cost: 4 * 0.35 + 0.05,
   },
   {
     id: 23,
     provider: "google",
     ngpus: 4,
-    instance: "nvidia-tesla-p4",
+    instance: "n1-standard-1",
     gpu: "p4",
-    cost: 4 * 0.6,
+    cost: 4 * 0.6 + 0.05,
   },
   {
     id: 24,
     provider: "google",
     ngpus: 4,
-    instance: "nvidia-tesla-v100",
+    instance: "n1-standard-1",
     gpu: "v100",
-    cost: 4 * 2.48,
+    cost: 4 * 2.48 + 0.05,
   },
   {
     id: 25,
     provider: "google",
     ngpus: 4,
-    instance: "nvidia-tesla-p100",
+    instance: "n1-standard-1",
     gpu: "p100",
-    cost: 4 * 1.46,
+    cost: 4 * 1.46 + 0.05,
   },
-  {
-    id: 26,
-    provider: "google",
-    ngpus: 4,
-    instance: "nvidia-tesla-k80",
-    gpu: "k80",
-    cost: 4 * 0.45,
-  },
-
   {
     id: 27,
     provider: "aws",
@@ -236,11 +210,43 @@ export const cloudInstances = [
   },
   {
     id: 28,
-    provider: "aws",
+    provider: "azure",
+    ngpus: 2,
+    instance: "NC12s v2",
+    gpu: "p100",
+    cost: 4.14,
+  },
+  {
+    id: 29,
+    provider: "azure",
     ngpus: 4,
-    instance: "g4dn.12xlarge",
+    instance: "NC24rs v2",
+    gpu: "p100",
+    cost: 9.1080,
+  },
+  {
+    id: 30,
+    provider: "azure",
+    ngpus: 2,
+    instance: "NC12s v3",
+    gpu: "v100",
+    cost: 6.12,
+  },
+  {
+    id: 31,
+    provider: "azure",
+    ngpus: 4,
+    instance: "NC24rs v3",
+    gpu: "v100",
+    cost: 13.46,
+  },
+  {
+    id: 32,
+    provider: "azure",
+    ngpus: 4,
+    instance: "NC64as T4 v3",
     gpu: "t4",
-    cost: 3.912,
+    cost: 4.352,
   },
 ];
 
@@ -263,11 +269,15 @@ export const cloudProviders = {
 }
 
 export const gpuPropertyList = [
-  { name: "t4", vmem: 16 },
-  { name: "p4", vmem: 8 },
-  { name: "v100", vmem: 16 },
   { name: "p100", vmem: 16 },
-  { name: "a100", vmem: 40 },
-  { name: "k80", vmem: 24 },
+  { name: "p4000", vmem: 8 },
+  { name: "rtx2070", vmem: 8 },
   { name: "rtx2080ti", vmem: 11 },
+  { name: "t4", vmem: 16 },
+  { name: "v100", vmem: 16 },
+  { name: "a100", vmem: 40 },
+  { name: "rtx3090", vmem: 24},
+  { name: "a40", vmem: 48},
+  { name: "a4000", vmem: 16},
+  { name: "rtx4000", vmem: 8}
 ];
