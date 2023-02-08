f951f31ec926fbe2ac01a4a64a40b57eade1deda
John Calderon <johncalesp@gmail.com>
2023-02-08 11:33:32 -0500

    fixed the warnings thrown by react

--- skyline-vscode/react-ui/src/ProjectInfo.js
+++ skyline-vscode/react-ui/src/ProjectInfo.js
@@ -1,16 +1,38 @@
 
-import Subheader from './Subheader';
+import React from "react";
+import styled from "styled-components";
 
-export default function ProjectInfo({projectRoot, entryPoint}) {
-    return (
+export default function ProjectInfo({ projectRoot, entryPoint }) {
+  return (
+    <>
+      <Wrapper>
         <div className="innpv-memory innpv-subpanel">
-            {/* <Subheader icon="database">Project Info</Subheader> */}
-            <div className="innpv-subpanel-content">
-                <table>
-                    <tr><td>Project Root</td> <td><code>{projectRoot}</code></td></tr>
-                    <tr><td>Entry Point</td> <td><code>{entryPoint}</code></td></tr>
-                </table>
+          <div className="innpv-subpanel-content">
+            <div className="info-main">
+              <div className="info-body">
+                <h6>Project Root</h6> <code>{projectRoot}</code>
+              </div>
+              <div className="info-body">
+                <h6>Entry Point</h6> <code>{entryPoint}</code>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </Wrapper>
+    </>
+  );
 }
+
+const Wrapper = styled.div`
+  .info-main {
+    display: flex;
+    flex-direction: column;
+  }
+  .info-body {
+    display: flex;
+    flex-direction: row;
+    h6 {
+        margin-right: 1rem;
+    }
+  }
+`;
