34951d55f98b8714f560fee585889398b316a6f6
John Calderon <johncalesp@gmail.com>
2023-02-10 10:21:40 -0500

    finished PerfBarContainer | MemoryPerfBar | PerfBar

--- skyline-vscode/react-ui/src/MemoryPerfBar.js
+++ skyline-vscode/react-ui/src/MemoryPerfBar.js
@@ -1,31 +1,13 @@
-'use babel';
-
-// import path from 'path';
 import React from 'react';
 
 import PerfBar from './PerfBar';
 import App from './App';
-import {toReadableByteSize} from './utils';
 
-class MemoryPerfBar extends React.Component {
-  constructor(props) {
-    super(props);
-    // this._renderPerfHints = this._renderPerfHints.bind(this);
-    // this._onClick = this._onClick.bind(this);
-    this._onDoubleClick = this._onDoubleClick.bind(this);
-    // this._onActiveChange = this._onActiveChange.bind(this);
-  }
+const MemoryPerfBar = (props) => {
 
-  // _generateTooltipHTML() {
-  //   const {memoryNode, overallPct} = this.props;
-  //   return `<strong>${memoryNode.name}</strong><br/>` +
-  //     `${toReadableByteSize(memoryNode.sizeBytes)}<br/>` +
-  //     `${overallPct.toFixed(2)}%`;
-  // }
-
-  _onDoubleClick() {
-    if(this.props.elem.file_refs && this.props.elem.file_refs.length > 0 && App.vscodeApi){
-      let file_context = this.props.elem.file_refs[0];
+  const onDoubleClick = () => {
+    if(props.elem.file_refs && props.elem.file_refs.length > 0 && App.vscodeApi){
+      let file_context = props.elem.file_refs[0];
 
       App.vscodeApi.postMessage({
         command: "highlight_source_line",
@@ -35,25 +17,18 @@ class MemoryPerfBar extends React.Component {
     }
   }
 
-  render() {
-    // const {memoryNode, editorsByPath, ...rest} = this.props;
-    const {tooltipHTML, ...rest} = this.props;
-    return (
-      <PerfBar
-        clickable={true}
-        renderPerfHints={()=>{}}
-        tooltipHTML={tooltipHTML}
-        // onClick={this._onClick}
-        onDoubleClick={this._onDoubleClick}
-        // onActiveChange={this._onActiveChange}
-        {...rest}
-      />
-    );
-  }
-}
+  const {tooltipHTML, ...rest} = props;
 
-MemoryPerfBar.defaultProps = {
-  editors: [],
-};
+  return (
+    <PerfBar
+      clickable={true}
+      renderPerfHints={()=>{}}
+      tooltipHTML={tooltipHTML}
+      onDoubleClick={onDoubleClick}
+      {...rest}
+    />
+  );
+
+}
 
 export default MemoryPerfBar;
