417273c0f3eef992c12b7a8f913a49cf3839fa9e
John Calderon <johncalesp@gmail.com>
2023-02-24 16:57:10 -0500

    reducing size using GENERATE_SOURCEMAP=false

---
+++ skyline-vscode/react-ui/analyze.js
@@ -0,0 +1,14 @@
+const webpack = require("webpack")
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const webpackConfigProd = require("react-scripts/config/webpack.config")(
+  "production"
+)
+
+webpackConfigProd.plugins.push(new BundleAnalyzerPlugin())
+
+webpack(webpackConfigProd, (err, stats) => {
+  if (err || stats.hasErrors()) {
+    console.error(err)
+  }
+})
