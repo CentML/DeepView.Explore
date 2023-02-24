a2fc65485da9dee57b137963939f8be39a351ecc
John Calderon <81483067+johncalesp@users.noreply.github.com>
2023-02-23 20:16:25 -0500

    Model layers height (#43)
    * changed height
    
    * changed habitat chart
    
    * alternative horizontal bar graph
    
    * refactored habitat graph

--- skyline-vscode/react-ui/src/App.js
+++ skyline-vscode/react-ui/src/App.js
@@ -1,23 +1,22 @@
-import './App.css';
-import './styles.css';
+import "./App.css";
+import "./styles.css";
 
-import React, { useState, useEffect } from 'react';
-import { Alert, Button, Card, Tab, Tabs, Container } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Alert, Button, Card, Tab, Tabs, Container } from "react-bootstrap";
 
-import ProjectInfo from './components/ProjectInfo'
-import Habitat from './sections/Habitat'
-import DeploymentTab from './sections/DeploymentTab'
-import WelcomeScreen from './sections/WelcomeScreen';
-import PerfBarContainer from './sections/PerfBarContainer';
+import ProjectInfo from "./components/ProjectInfo";
+import Habitat from "./sections/Habitat";
+import DeploymentTab from "./sections/DeploymentTab";
+import WelcomeScreen from "./sections/WelcomeScreen";
+import PerfBarContainer from "./sections/PerfBarContainer";
 
-import ReactTooltip from 'react-tooltip';
+import ReactTooltip from "react-tooltip";
 
-
-import { computePercentage, getTraceByLevel } from './utils/utils';
-import { profiling_data } from './data/mock_data';
-import EnergyConsumption from './sections/EnergyConsumption';
-import Iterations from './sections/Iterations';
-import MemThroughputContainer from './sections/MemThroughputContainer';
+import { computePercentage, getTraceByLevel } from "./utils/utils";
+import { profiling_data } from "./data/mock_data";
+import EnergyConsumption from "./sections/EnergyConsumption";
+import Iterations from "./sections/Iterations";
+import MemThroughputContainer from "./sections/MemThroughputContainer";
 
 // https://stackoverflow.com/questions/54135313/webview-extension-in-typescript
 /**
@@ -27,189 +26,212 @@ import MemThroughputContainer from './sections/MemThroughputContainer';
  * @returns The VSCode API handle
  */
 function acquireApi() {
-    // if (typeof this.acquireApi.api == 'undefined') {
-    if (typeof acquireApi.api === 'undefined') {
-        if (typeof acquireVsCodeApi === "function") {
-            let f = window['acquireVsCodeApi'];
-            let a = f();
-            acquireApi.api = a;
-        } else {
-            acquireApi.api = null;
-        }
+  // if (typeof this.acquireApi.api == 'undefined') {
+  if (typeof acquireApi.api === "undefined") {
+    if (typeof acquireVsCodeApi === "function") {
+      let f = window["acquireVsCodeApi"];
+      let a = f();
+      acquireApi.api = a;
+    } else {
+      acquireApi.api = null;
     }
+  }
 
-    return acquireApi.api;
+  return acquireApi.api;
 }
 
 function restartProfiling() {
-    console.log("restartProfiling");
-    let vscode = App.vscodeApi;
-    vscode.postMessage({
-        command: "restart_profiling_clicked"
-    });
+  console.log("restartProfiling");
+  let vscode = App.vscodeApi;
+  vscode.postMessage({
+    command: "restart_profiling_clicked",
+  });
 }
 
 const sendMock = false;
 
 function App() {
-    const [analysisState, setAnalysisState] = useState();
-    const [textChanged, setTextChanged] = useState(false);
-    const [timeBreakDown, setTimeBreakdown] = useState([]);
+  const [analysisState, setAnalysisState] = useState();
+  const [textChanged, setTextChanged] = useState(false);
+  const [timeBreakDown, setTimeBreakdown] = useState([]);
 
-    const [vscodeApi, setVscodeApi] = useState(acquireApi());
-    const [errorText, setErrorText] = useState();
-    const [connectionStatus, setConnectionStatus] = useState(false);
-    const [numIterations,setNumIterations] = useState(100000);
+  const [vscodeApi, setVscodeApi] = useState(acquireApi());
+  const [errorText, setErrorText] = useState();
+  const [connectionStatus, setConnectionStatus] = useState(false);
+  const [numIterations, setNumIterations] = useState(100000);
 
-    App.vscodeApi = vscodeApi;
+  App.vscodeApi = vscodeApi;
 
-    const resetApp =  function() {
-        setErrorText("");
-        setAnalysisState(undefined);
+  const resetApp = function () {
+    setErrorText("");
+    setAnalysisState(undefined);
+  };
+
+  const connect = function () {
+    resetApp();
+    let vscode = App.vscodeApi;
+    if (vscode) {
+      vscode.postMessage({
+        command: "connect",
+      });
+    }
+  };
+
+  const processAnalysisState = function (state) {
+    setAnalysisState(state);
+    if (state.breakdown) {
+      let operation_tree = state.breakdown.operation_tree;
+      let { coarse, fine } = getTraceByLevel(operation_tree);
+      setTimeBreakdown({
+        coarse: computePercentage(
+          coarse,
+          state.breakdown.iteration_run_time_ms
+        ),
+        fine: computePercentage(fine, state.breakdown.iteration_run_time_ms),
+      });
+      ReactTooltip.rebuild();
+    }
+  };
+
+  useEffect(function () {
+    window.addEventListener("message", (event) => {
+      if (event.data["message_type"] === "connection") {
+        setConnectionStatus(event.data["status"]);
+      } else if (event.data["message_type"] === "analysis") {
+        processAnalysisState(event.data);
+      } else if (event.data["message_type"] === "text_change") {
+        setTextChanged(true);
+      } else if (event.data["message_type"] === "error") {
+        setErrorText(event.data["error_text"]);
+      }
+    });
+
+    if (sendMock) {
+      setTimeout(() => {
+        const mockResponse = profiling_data;
+        processAnalysisState(mockResponse);
+      }, 1000);
     }
 
-    const connect = function() {
-        resetApp();
-        let vscode = App.vscodeApi;
-        if(vscode){
-            vscode.postMessage({
-                command: "connect"
-            });
-        }
-    }
+    return () => {
+      window.removeEventListener("message", () => {}); //remove event listener before re-render to avoid memory leaks
+    };
+  }, []);
 
-    const processAnalysisState = function (state) {
-        setAnalysisState(state);
-        if (state.breakdown) {
-            let operation_tree = state.breakdown.operation_tree;
-            let { coarse, fine } = getTraceByLevel(operation_tree);
-            setTimeBreakdown({ 
-                coarse: computePercentage(coarse, state.breakdown.iteration_run_time_ms),
-                fine: computePercentage(fine, state.breakdown.iteration_run_time_ms)
-            });
-            ReactTooltip.rebuild();
-        }
-    }
-
-    useEffect(function () {
-        window.addEventListener('message', event => {
-            if (event.data['message_type'] === "connection") {
-                setConnectionStatus(event.data['status']);
-            } else if (event.data['message_type'] === "analysis") {
-                processAnalysisState(event.data);
-            } else if (event.data['message_type'] === "text_change") {
-                setTextChanged(true);
-            } else if (event.data['message_type'] === 'error') {
-                setErrorText(event.data['error_text']);
-            }
-        });
-
-        
-
-        if (sendMock) {
-            setTimeout(() => {
-                const mockResponse = profiling_data;
-                processAnalysisState(mockResponse);
-            }, 1000);
-        }
-
-        return() => {
-            window.removeEventListener("message",()=>{}) //remove event listener before re-render to avoid memory leaks
-        }
-    }, []);
-
-    if (!connectionStatus && !sendMock) {
-        return (
-            <>
-            <Alert variant="danger">
-            <Alert.Heading>Connection Error</Alert.Heading>
-            <p>
-                Connection has been lost to the profiler. Please reconnect the profiler and double check your ports then click connect
-            </p>
-            <Button onClick={connect}>Reconnect</Button>
-            </Alert>
-            </>
-        )
-    }
-    if (errorText) {
-        return (
-            <>
-            <Alert variant="danger">
-            <Alert.Heading>Analysis Error</Alert.Heading>
-            <p>
-                An error has occurred during analysis. This could be a problem with Skyline or possibly your code. For more information, refer to the detailed message below:
-            </p>
-            <hr />
-            <p className="mb-0">
-                <code>
-                    {errorText}
-                </code>
-            </p>
-            <hr />
-            <Button onClick={restartProfiling}>Restart Profiling</Button>
-            </Alert>
-            </>
-        )
-    }
-    else if (analysisState && analysisState['throughput'] && Object.keys(analysisState['throughput']).length > 0)
-    {
-        return (
-            <>
-                <Container fluid>
-                <Card>
-                    <Card.Header>Project Information</Card.Header>
-                    <Card.Body>
-                            <ProjectInfo
-                                projectRoot={analysisState['project_root']}
-                                entryPoint={analysisState['project_entry_point']}
-                            />
-                        { textChanged && 
-                        <Alert key='info' variant='info'>
-                            Change is detected in the project. <Button size='sm' onClick={restartProfiling}>Restart Profiling</Button>
-                        </Alert>
-                        }
-                    </Card.Body>
-                </Card>
-                <Iterations setNumIterations={setNumIterations}/>
-                <br></br>
-                <Tabs defaultActiveKey="profiling" className="mb-3">
-                    <Tab eventKey="profiling" title="Profiling">
-                    <div className='innpv-contents-columns'>
-                    <div className='innpv-perfbar-contents'>
-                        <PerfBarContainer 
-                            labels={timeBreakDown.coarse ? timeBreakDown.coarse.map(elem => {
-                                return {
-                                    label: elem["percentage"] > 10 ? elem["name"] : "", 
-                                    percentage: elem["percentage"],
-                                    clickable: false
-                                }
-                            }) : []}
-                            renderPerfBars={timeBreakDown.fine}
-                        />
-                        <ReactTooltip type="info" effect="float" html={true}/>
-                    </div>
-                    <div className="innpv-contents-subrows">
-                    <MemThroughputContainer analysisState={analysisState} SENDMOCK={sendMock} />
-                    <Habitat habitatData={analysisState['habitat']}/>
-                    <EnergyConsumption energyData={analysisState['energy']} numIterations={numIterations}/>
-                    </div>
-                    </div>
-                    </Tab>
-                    <Tab eventKey="deploy" title="Deployment">
-                        <DeploymentTab numIterations={numIterations} habitatData={analysisState['habitat']}/>
-                    </Tab>
-                </Tabs>
-                </Container>
-            </>
-        );
-    }
-    else {
-        return (
-            <>
-                <WelcomeScreen analysisState={analysisState} vscodeApi={vscodeApi}></WelcomeScreen>
-            </>
-        );
-    }
+  if (!connectionStatus && !sendMock) {
+    return (
+      <>
+        <Alert variant="danger">
+          <Alert.Heading>Connection Error</Alert.Heading>
+          <p>
+            Connection has been lost to the profiler. Please reconnect the
+            profiler and double check your ports then click connect
+          </p>
+          <Button onClick={connect}>Reconnect</Button>
+        </Alert>
+      </>
+    );
+  }
+  if (errorText) {
+    return (
+      <>
+        <Alert variant="danger">
+          <Alert.Heading>Analysis Error</Alert.Heading>
+          <p>
+            An error has occurred during analysis. This could be a problem with
+            Skyline or possibly your code. For more information, refer to the
+            detailed message below:
+          </p>
+          <hr />
+          <p className="mb-0">
+            <code>{errorText}</code>
+          </p>
+          <hr />
+          <Button onClick={restartProfiling}>Restart Profiling</Button>
+        </Alert>
+      </>
+    );
+  } else if (
+    analysisState &&
+    analysisState["throughput"] &&
+    Object.keys(analysisState["throughput"]).length > 0
+  ) {
+    return (
+      <>
+        <Container fluid>
+          <Card>
+            <Card.Header>Project Information</Card.Header>
+            <Card.Body>
+              <ProjectInfo
+                projectRoot={analysisState["project_root"]}
+                entryPoint={analysisState["project_entry_point"]}
+              />
+              {textChanged && (
+                <Alert key="info" variant="info">
+                  Change is detected in the project.{" "}
+                  <Button size="sm" onClick={restartProfiling}>
+                    Restart Profiling
+                  </Button>
+                </Alert>
+              )}
+            </Card.Body>
+          </Card>
+          <Iterations setNumIterations={setNumIterations} />
+          <br></br>
+          <Tabs defaultActiveKey="profiling" className="mb-3">
+            <Tab eventKey="profiling" title="Profiling">
+              <div className="innpv-contents-columns">
+                <div className="innpv-perfbar-contents">
+                  <PerfBarContainer
+                    labels={
+                      timeBreakDown.coarse
+                        ? timeBreakDown.coarse.map((elem) => {
+                            return {
+                              label:
+                                elem["percentage"] > 10 ? elem["name"] : "",
+                              percentage: elem["percentage"],
+                              clickable: false,
+                            };
+                          })
+                        : []
+                    }
+                    renderPerfBars={timeBreakDown.fine}
+                  />
+                  <ReactTooltip type="info" effect="float" html={true} />
+                </div>
+                <div className="innpv-contents-subrows">
+                  <MemThroughputContainer
+                    analysisState={analysisState}
+                    SENDMOCK={sendMock}
+                  />
+                  <Habitat habitatData={analysisState["habitat"]} />
+                  <EnergyConsumption
+                    energyData={analysisState["energy"]}
+                    numIterations={numIterations}
+                  />
+                </div>
+              </div>
+            </Tab>
+            <Tab eventKey="deploy" title="Deployment">
+              <DeploymentTab
+                numIterations={numIterations}
+                habitatData={analysisState["habitat"]}
+              />
+            </Tab>
+          </Tabs>
+        </Container>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <WelcomeScreen
+          analysisState={analysisState}
+          vscodeApi={vscodeApi}
+        ></WelcomeScreen>
+      </>
+    );
+  }
 }
 
 export default App;
--- skyline-vscode/react-ui/src/sections/Habitat.js
+++ skyline-vscode/react-ui/src/sections/Habitat.js
@@ -1,60 +1,36 @@
+import React from "react";
+
 import Subheader from "../components/Subheader";
-import { Container, Row, Spinner, Card } from "react-bootstrap";
-import BarGraph from "../components/HorizontalBarGraph";
+import { Container, Row,Spinner, Card } from "react-bootstrap";
+import { HabitatScatterGraph } from "../components/ScatterGraph";
 
 export default function Habitat({ habitatData }) {
-  // The colors used for the visualization
-  // The first n-1 colors should follow a blue gradient while the last
-  // color is used for the current device.
-  const colors = [
-    "#7986cb",
-    "#5c6bc0",
-    "#3f51b5",
-    "#3949ab",
-    "#303f9f",
-    "#283593",
-    "#1a237e",
-    "#ffc300",
-  ];
-
-  let habitatProperties = [];
-
-  for (var i = 0; i < habitatData.length; i++) {
-    habitatProperties.push({
-      y: habitatData[i][0],
-      x: habitatData[i][1].toFixed(2),
-      fill:
-        habitatData[i][0] === "source"
-          ? colors[colors.length - 1]
-          : colors[i % (colors.length - 1)],
-    });
-  }
 
   return (
-    <div className="innpv-memory innpv-subpanel">
-      <Subheader icon="database">Habitat</Subheader>
-      <div className="innpv-subpanel-content">
-        {habitatData.length === 0 && (
-          <Container fluid>
-            <Row className="justify-content-md-center">
-              <Card>
-                <Card.Body>
-                  <Spinner animation="border" size="sm" /> Loading Habitat
-                  predictions.
-                </Card.Body>
-              </Card>
-            </Row>
-          </Container>
-        )}
-        {habitatData !== [] && (
-          <BarGraph
-            data={habitatProperties}
-            height={50*habitatProperties.length}
-            xlabel={"Predicted Runtime (ms)"}
-            ylabel={""}
-          />
-        )}
+    <>
+      <div className="innpv-memory innpv-subpanel">
+        <Subheader icon="database">Habitat</Subheader>
+        <div className="innpv-subpanel-content">
+          {habitatData.length === 0 ? (
+            <Container fluid>
+              <Row className="justify-content-md-center">
+                <Card>
+                  <Card.Body>
+                    <Spinner animation="border" size="sm" /> Loading Habitat
+                    predictions.
+                  </Card.Body>
+                </Card>
+              </Row>
+            </Container>
+          ):
+          (
+            <HabitatScatterGraph
+              habitatData={habitatData}
+              height={500}
+            />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
