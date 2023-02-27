7e90952b77dae305a919b914e347ce1fed567960
John Calderon <johncalesp@gmail.com>
2023-02-27 15:31:02 -0500

    changed build script

--- scripts/build_vsix.sh
+++ scripts/build_vsix.sh
@@ -2,18 +2,15 @@
 
 # Build react UI
 pushd skyline-vscode/react-ui && \
-    npm install --legacy-peer-deps && \
-    CI=false npm run build && \
+    npm install && \
+    npm run build && \
     popd;
 
 # Build backend
-# TODO: Replace "npm install react" with a proper fix in package.json
 pushd skyline-vscode && \
-    npm install --legacy-peer-deps  && \
+    npm install && \
     pushd src/protobuf && 
     make && make old && \
     popd && \
-    npm install react react-dom && \
-    yes | vsce package && \
+    vsce package && \
     popd;
-
