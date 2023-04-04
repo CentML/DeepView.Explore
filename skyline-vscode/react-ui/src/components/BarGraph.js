58a9cbff6bf22ee44462e22232f32abcbf90aa78
michaelshin <michaelshin@users.noreply.github.com>
2023-04-04 11:35:39 -0400

    Revert BarGraph.js

--- skyline-vscode/react-ui/src/components/BarGraph.js
+++ skyline-vscode/react-ui/src/components/BarGraph.js
@@ -9,46 +9,40 @@ import {
 } from "recharts";
 
 
-export const HorizontalBarGraph = ({ data, height, xlabel, ylabel, color, onBarClick }) => {
+export const HorizontalBarGraph = ({ data, height, xlabel, ylabel, color }) => {
   let upperLimit = 0;
-  let longestLabel = 0;
   data.forEach((element) => {
-    const value = parseFloat(Number(element.x));
-    upperLimit = value > upperLimit ? value : upperLimit;
-    const currLabelLength = element.y.length;
-    longestLabel = currLabelLength > longestLabel ? currLabelLength : longestLabel;
+    const predictedTime = parseFloat(Number(element.x));
+    upperLimit = predictedTime > upperLimit ? predictedTime : upperLimit;
   });
   upperLimit *= 1.1;
   data.sort((a, b) => a.x - b.x);
   return (
-    <ResponsiveContainer width="100%" height={height}>
+    <ResponsiveContainer width="80%" height={height}>
       <BarChart
         layout="vertical"
         data={data}
-        margin={{ top: 20, right: 20, left: 30, bottom: 20 }}
+        margin={{ top: 10, right: 30, left: 50, bottom: 50 }}
         barCategoryGap={10}
       >
         <XAxis
           type="number"
-          dataKey="x"
-          domain={[0, 'auto']}
+          domain={[0, Math.floor(upperLimit)]}
           label={{ value: xlabel, position: "insideBottom", offset: -15 }}
-          allowDecimals={true}
         />
         <YAxis
+          width={70}
           type="category"
-          width={longestLabel*7}
           dataKey="y"
           label={{
             value: ylabel,
             angle: -90,
             position: "insideLeft",
-            offset: -70,
+            offset: -35,
           }}
         />
-        <Tooltip
-        formatter={(value) => [value.toFixed(4), xlabel]} />
-        <Bar dataKey="x" fill={color} onClick={onBarClick} />
+        <Tooltip />
+        <Bar dataKey="x" fill={color} />
       </BarChart>
     </ResponsiveContainer>
   );
