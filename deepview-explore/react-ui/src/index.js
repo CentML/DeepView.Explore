7e86f298dda6e10d2f2faddd35747fde6029965b
Saksham Saini <sakshamsaini18@gmail.com>
2023-04-20 11:57:37 -0400

    Add bugsnag

--- deepview-explore/react-ui/src/index.js
+++ deepview-explore/react-ui/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+
+Bugsnag.start({
+  apiKey: "98f9219663e35fb3b4487b709a00290d",
+  plugins: [new BugsnagPluginReact()],
+});
+
+const ErrorBoundary = Bugsnag.getPlugin("react").createErrorBoundary(React);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
