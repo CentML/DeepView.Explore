820d0ed385e767077e00fe755631b87fe6fdfa96
John Calderon <81483067+johncalesp@users.noreply.github.com>
2024-03-05 16:57:37 -0500

    changed ProjectInfo to fix color issues (#86)
    Co-authored-by: John Calderon <john.calderon@centml.ai>

--- deepview-explore/react-ui/src/components/ProjectInfo.js
+++ deepview-explore/react-ui/src/components/ProjectInfo.js
@@ -1,44 +1,27 @@
-
 import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
-
 const ProjectInfo = () => {
-  const {analysisState} = useSelector((state) => state.analysisStateSliceReducer);
+  const { analysisState } = useSelector(
+    (state) => state.analysisStateSliceReducer
+  );
   const projectRoot = analysisState["project_root"];
   const entryPoint = analysisState["project_entry_point"];
   return (
     <>
       <Wrapper>
-        <div className="innpv-memory innpv-subpanel">
-          <div className="innpv-subpanel-content">
-            <div className="info-main">
-              <div className="info-body">
-                <h6>Project Root</h6> <code>{projectRoot}</code>
-              </div>
-              <div className="info-body">
-                <h6>Entry Point</h6> <code>{entryPoint}</code>
-              </div>
-            </div>
+        <div className="innpv-memory innpv-subpanel innpv-subpanel-content">
+          <div>
+            <strong>Project Root</strong> &nbsp; {projectRoot}
+          </div>
+          <div>
+            <strong>Entry Point</strong> &nbsp;&nbsp;&nbsp; {entryPoint}
           </div>
         </div>
       </Wrapper>
     </>
   );
-}
+};
 
-
-const Wrapper = styled.div`
-.info-main {
-  display: flex;
-  flex-direction: column;
-}
-.info-body {
-  display: flex;
-  flex-direction: row;
-  h6 {
-    margin-right: 1rem;
-  }
-}
-`;
+const Wrapper = styled.div``;
 export default ProjectInfo;
